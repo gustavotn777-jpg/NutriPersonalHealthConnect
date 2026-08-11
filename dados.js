14b92a736d272efcd4c23494305bd2c513299d79
gustavotn777-jpg <gustavo.t.n777@gmail.com>
2026-08-11 13:14:48 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,17 @@
+const dados = {
+  saude: [],
+  treinos: []
+};
+
+function salvarDados() {
+  localStorage.setItem("nutriPersonalDados", JSON.stringify(dados));
+}
+
+function carregarDados() {
+  const salvos = localStorage.getItem("nutriPersonalDados");
+  if (salvos) {
+    Object.assign(dados, JSON.parse(salvos));
+  }
+}
+
+carregarDados();
